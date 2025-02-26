5a9c29a591fef5e42230b12a2b6ef077d01fed1c
kaeka <Sulis347@gmail.com>
2025-02-26 16:38:16 +0900

    try modify visibility

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Menu
 
-# Register your models here.
-admin.site.register(Menu)
+
+
+class MenuModified(admin.ModelAdmin):
+  list_display = ("meal_name", "showmeal")
 
 # Register your models here.
+# Register your models here.
+admin.site.register(Menu,MenuModified)
