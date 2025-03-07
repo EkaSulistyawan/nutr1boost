506fdaa19c138beef716d57b553cb7da91caf59c
kaeka <Sulis347@gmail.com>
2025-03-07 13:35:58 +0900

    add trained model

--- efficientNetDev/train_efficientnet.py
+++ efficientNetDev/train_efficientnet.py
@@ -28,63 +28,62 @@ class MenuDataset(Dataset):
         return image,label
 
 batch_size = 8
-num_epochs = 100
+num_epochs = 20
 
-image_dir   = "./object_classification_batch_1/"
-label_path  = "./object_classification_batch_1.csv"
 class_path  = "./classes.txt"
-trainidx    = 6
-
-# make label's hot encoding
-label   = pd.read_csv(label_path,header=None,names=['filename','class']).reset_index(drop=True).set_index('class')
+trainidx    = 0
+# load model
+# Define EfficientNet-B2
 # # load the classes, make classes hot encoded
 classes = pd.read_csv(class_path,header=None,names=['class'])
 classes = classes.join(pd.get_dummies(classes)).set_index('class')
-label   = label.join(classes,how='inner').reset_index(drop=True).set_index('filename')
-
-transform = transforms.Compose([
-    transforms.RandomResizedCrop(224,scale=(0.8,1.0)),
-    transforms.ToTensor(),
-    transforms.ColorJitter(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
-dataset = MenuDataset(image_dir,label,transform)
-menuloader = DataLoader(dataset,batch_size=batch_size,shuffle=False)
-
-
-# load model
-# Define EfficientNet-B2
-model = models.efficientnet_b2(pretrained=False)
+model = models.efficientnet_b2(pretrained=True)
 num_classes = len(classes)
 model.classifier[1] = nn.Linear(model.classifier[1].in_features,num_classes)
 
-model.load_state_dict(torch.load(f"./trained_model/train{trainidx}_100.pth"))
+model.load_state_dict(torch.load(f"./trained_model/train{trainidx}.pth"))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-# training
-for epoch in range(num_epochs):
-    model.train()
-    avg_loss = 0 
-    imtrain,labeltrain = next(iter(menuloader))
-    tq =tqdm(menuloader)
-    for imtrain, labeltrain in tq:
-        optimizer.zero_grad()
+for batch_idx in range(1,5):
+    image_dir   = f"./object_classification_batch_{batch_idx}/"
+    label_path  = f"./object_classification_batch_{batch_idx}.csv"
+    # make label's hot encoding
+    label   = pd.read_csv(label_path,header=None,names=['filename','class']).reset_index(drop=True).set_index('class')
+    label   = label.join(classes,how='inner').reset_index(drop=True).set_index('filename')
 
-        imtrain, labeltrain = imtrain.to(device),labeltrain.to(device)
-        outp = model(imtrain)
+    transform = transforms.Compose([
+        transforms.RandomResizedCrop(224,scale=(0.8,1.0)),
+        transforms.ToTensor(),
+        transforms.ColorJitter(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-        loss = criterion(outp,labeltrain)
-        loss.backward()
-        optimizer.step()
+    dataset = MenuDataset(image_dir,label,transform)
+    menuloader = DataLoader(dataset,batch_size=batch_size,shuffle=False)
 
-        tq.set_postfix(loss=loss.item())
+    # training
+    for epoch in range(num_epochs):
+        model.train()
+        avg_loss = 0 
+        imtrain,labeltrain = next(iter(menuloader))
+        tq =tqdm(menuloader)
+        for imtrain, labeltrain in tq:
+            optimizer.zero_grad()
+
+            imtrain, labeltrain = imtrain.to(device),labeltrain.to(device)
+            outp = model(imtrain)
+
+            loss = criterion(outp,labeltrain)
+            loss.backward()
+            optimizer.step()
+
+            tq.set_postfix(loss=loss.item())
 
 
-torch.save(model.state_dict(), f"./trained_model/train{trainidx+1}_100.pth")
+torch.save(model.state_dict(), f"./trained_model/train{trainidx+1}.pth")
